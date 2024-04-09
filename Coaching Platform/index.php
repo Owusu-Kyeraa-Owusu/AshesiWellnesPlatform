83f91046e234ee83466d6bc21e4befa9415c0158
Owusu-Kyeraa-Owusu <165173012+Owusu-Kyeraa-Owusu@users.noreply.github.com>
2024-04-09 15:04:33 +0000

    Add files via upload

---
+++ Coaching Platform/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ashesi Wellness Coach and Counseling Platform</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Ashesi Wellness Coach and Counseling Platform</h1>
+        <div class="logo">
+            <!-- Insert your platform logo here -->
+            <img src="../background_images/logo.png" alt="Platform Logo">
+        </div>
+        
+        <div class="buttons">
+            <a href="../Login/login.php" class="login-button">Login</a>
+            <a href="../Login/register.php" class="signup-button">Sign Up</a>
+      
+           
+            
+        </div>
+
+
+
+    </div>
+
+
+    
+</body>
+</html>
